660b8343676005f417528a8ba41790b59ffd6ef6
marco <marco3jp+git@gmail.com>
2021-05-05 04:12:33 +0900

    飲酒フォームを調整

--- components/organisms/TodayDrunkForm.vue
+++ components/organisms/TodayDrunkForm.vue
@@ -1,22 +1,30 @@
 <template>
   <div>
     <h2>飲酒登録</h2>
-    <div>
-      <label for="base-alcohol-percent">ベースアルコールの度数</label>
-      <input type="number" id="base-alcohol-percent">
-    </div>
-    <div>
-      <label for="base-alcohol-milliliter">ベースアルコールの量</label>
-      <input type="number" id="base-alcohol-milliliter">
-    </div>
-    <div>
-      <label for="total-drink-milliliter">アルコール飲料の全体量</label>
-      <input type="number" id="total-drink-milliliter">
-    </div>
-    <div>
-      <label for="ethanol-milliliter">エタノールの量</label>
-      <input type="number" id="ethanol-milliliter">
-    </div>
+    <select>
+      <option>5% 350ml</option>
+      <option>9% 350ml</option>
+      <option>15% 180ml</option>
+    </select>
+    <form>
+      <div>
+        <label for="base-alcohol-percent">ベースアルコールの度数</label>
+        <input type="number" id="base-alcohol-percent">
+      </div>
+      <div>
+        <label for="base-alcohol-milliliter">ベースアルコールの量</label>
+        <input type="number" id="base-alcohol-milliliter">
+      </div>
+      <div>
+        <label for="total-drink-milliliter">アルコール飲料の全体量</label>
+        <input type="number" id="total-drink-milliliter">
+      </div>
+      <div>
+        <label for="ethanol-milliliter">エタノールの量</label>
+        <input type="number" id="ethanol-milliliter">
+      </div>
+      <button type="button">登録</button>
+    </form>
   </div>
 </template>
 
