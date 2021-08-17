40eebe780c01e02eb87343b51ab97ed0ff79ff1d
marco <marco3jp+git@gmail.com>
2021-08-18 01:00:24 +0900

    フォームをそれっぽく作った

--- components/organisms/TodayDrunkQuickForm.vue
+++ components/organisms/TodayDrunkQuickForm.vue
@@ -1,29 +1,42 @@
 <template>
   <div class="today-drunk-quick-form">
-    <div class="today-drunk-quick-form__basic-form">
-      <label>
-        <span>アルコール度数(%)</span>
-        <input type="number" v-model="localLiquor.alcohol.rate" @input="calculateAlcoholQuantity">
-      </label>
-      <label>
-        <span>アルコール飲料の量(ml)</span>
-        <input type="number" v-model="localLiquor.quantity">
-      </label>
-      <label>
-        <span>アルコール量(ml)</span>
-        <input type="number" v-model="localLiquor.alcohol.quantity">
-      </label>
+    <form class="today-drunk-quick-form__basic-form basic-form" ref="basicForm">
+      <div class="basic-form__alcohol-rate">
+        <label>
+          <span>酒の度数(%)</span>
+          <input type="number" v-model="localLiquor.alcohol.rate"
+                 @input="calculateAlcoholQuantity">
+        </label>
+        <p> {{ alcoholRateErrorMessage }} </p>
+      </div>
 
-      <button @click="handleRecordDrunkLiquor">登録</button>
-    </div>
+      <div class="basic-form__quantity">
+        <label>
+          <span>酒の量(ml)</span>
+          <input type="number" step="10" min="0" v-model="localLiquor.quantity"
+                 @input="calculateAlcoholQuantity" ref="quantityInput">
+        </label>
+        <p>{{ quantityErrorMessage }}</p>
+      </div>
 
-    <hr>
+      <div class="basic-form__alcohol-quantity">
+        <label>
+          <span>アルコール量(ml)</span>
+          <input type="number" min="0" v-model="localLiquor.alcohol.quantity">
+        </label>
+        <p>{{ alcoholQuantityErrorMessage }}</p>
+      </div>
 
-    <div class="today-drunk-quick-form__templates">
+      <button class="basic-form__record-button" type="button" :disabled="!isValidForm" @click="handleRecordDrunkLiquor">
+        登録
+      </button>
+    </form>
+
+    <!--div class="today-drunk-quick-form__templates">
       <button v-for="liquorTemplate in liquorTemplates" @click="$emit('record-drunk', liquorTemplate)">
         {{liquorTemplate.name}}
       </button>
-    </div>
+    </div-->
   </div>
 </template>
 
@@ -33,73 +46,73 @@ import {Liquor} from "~/model/liquor";
 
 export default defineComponent({
   name: "TodayDrunkQuickForm",
-  setup(): {
-    liquorTemplates: Array<Liquor>
-  } {
-    return {
-      liquorTemplates: [
-        {
-          name: "ビール",
-          quantity: 350,
-          alcohol: {
-            quantity: 17.5,
-            rate: 5
-          },
-        },
-        {
-          name: "ストロングチューハイ",
-          quantity: 350,
-          alcohol: {
-            quantity: 31.5,
-            rate: 9,
-          }
-        },
-        {
-          name: "角ハイボール",
-          quantity: 350,
-          sources: [{
-            drink: {
-              name: "角瓶",
-              rate: 40,
-              type: "hard"
-            },
-            quantity: 70
-          }, {
-            drink: {
-              name: "ソーダ",
-              type: "soft"
-            },
-            quantity: 200
-          }, {
-            type: "ice",
-            amount: "山盛り"
-          }],
-          memo: "参考: https://www.suntory.co.jp/whisky/kakubin/kakuhigh/index.html"
-        }
-      ],
-
-    }
-  },
   data() {
     return {
       localLiquor: {
-        quantity: 0,
+        quantity: "0",
         alcohol: {
-          rate: 0,
-          quantity: 0
+          rate: "0",
+          quantity: "0"
         }
-      } as Liquor
+      },
+      resultMessage: ""
+    }
+  },
+  computed: {
+    alcoholRateErrorMessage(): string | undefined {
+      const v = Number(this.localLiquor.alcohol.rate)
+
+      if (this.localLiquor.alcohol.rate === "" || isNaN(v)) {
+        return "数字として扱えません"
+      } else if (v < 0 || v > 100) {
+        return "0~100で入力してください"
+      }
+    },
+    alcoholQuantityErrorMessage(): string | undefined {
+      const v = Number(this.localLiquor.alcohol.quantity)
+
+      if (this.localLiquor.alcohol.quantity === "" || isNaN(v)) {
+        return "数字として扱えません"
+      } else if (v < 0) {
+        return "0~で入力してください"
+      }
+    },
+    quantityErrorMessage(): string | undefined {
+      const v = Number(this.localLiquor.quantity);
+      if (this.localLiquor.quantity === "" || isNaN(v)) {
+        return "数字として扱えません"
+      } else if (v <= 0) {
+        return "1~で入力してください"
+      }
+    },
+    isValidForm() {
+      return !this.alcoholRateErrorMessage && !this.alcoholQuantityErrorMessage && !this.quantityErrorMessage
     }
   },
   methods: {
     handleRecordDrunkLiquor() {
       this.$emit('record-drunk', {
-        ...this.localLiquor
+        quantity: parseInt(this.localLiquor.quantity),
+        alcohol: {
+          rate: parseFloat(this.localLiquor.alcohol.rate),
+          quantity: parseInt(this.localLiquor.alcohol.quantity)
+        }
       } as Liquor)
+
+      this.resetForm();
     },
-    calculateAlcoholQuantity(){
-      if (this.localLiquor && this.localLiquor.alcohol && this.localLiquor.quantity) {
-        this.localLiquor.alcohol.quantity = this.localLiquor.quantity * (this.localLiquor.alcohol.rate / 100)
+    calculateAlcoholQuantity() {
+      if (this.isValidForm) {
+        this.localLiquor.alcohol.quantity = (Number(this.localLiquor.quantity) * (Number(this.localLiquor.alcohol.rate) / 100)).toString()
+      }
+    },
+    resetForm() {
+      this.localLiquor = {
+        quantity: "0",
+        alcohol: {
+          rate: "0",
+          quantity: "0"
+        }
       }
     }
   }
@@ -107,10 +120,43 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
-.today-drunk-quick-form{
-  &__basic-form{
+.today-drunk-quick-form {
+  &__basic-form {
     display: flex;
     flex-direction: column;
+    width: 100%;
+  }
+
+  &__templates {
+    margin-top: 16px;
+  }
+}
+
+.basic-form {
+  &__alcohol-rate, &__quantity, &__alcohol-quantity {
+    label {
+      height: 50px;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      input {
+        height: 100%;
+        width: 65%;
+        font-size: 2rem;
+      }
+    }
+
+    p {
+      height: 20px;
+      line-height: 20px;
+    }
+  }
+
+  &__record-button {
+    margin-top: 12px;
+    height: 50px;
   }
 }
 </style>
