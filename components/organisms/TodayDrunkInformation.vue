2899edc47725ba457d79b5fae3827fbb42139c01
marco <marco3jp+git@gmail.com>
2021-08-19 01:42:51 +0900

    過去12時間の記録の集計を見れるようにした

--- components/organisms/TodayDrunkInformation.vue
+++ components/organisms/TodayDrunkInformation.vue
@@ -1,23 +1,42 @@
 <template>
   <div>
-    <h2>現在のアルコール情報</h2>
+    <h2>過去12時間のアルコール情報</h2>
     <ul>
-      <li>エタノール量: 50ml
-        <ul>
-          <li>エタノール許容量: 70%</li>
-        </ul>
-      </li>
-      <li>水分摂取量: 500ml</li>
-      <li>飲酒時間: 2h 20m</li>
+      <li>アルコール量: {{ totalLiquorInfo.alcoholQuantity }}ml</li>
+      <li>飲酒量: {{ totalLiquorInfo.quantity }}ml</li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from '@nuxtjs/composition-api'
+import {recordQueryPayload} from "~/store/records";
+import {LiquorRecord} from "~/model/liquorRecord";
 
 export default defineComponent({
-  name: "TodayDrunkInformation"
+  name: "TodayDrunkInformation",
+  computed: {
+    totalLiquorInfo() {
+      const records: LiquorRecord[] = this.liquorRecordsLast12Hours;
+
+      return {
+        quantity: records.reduce(
+          (accumulator: number, current: LiquorRecord) => accumulator + (current.liquor.quantity ?? 0), 0
+        ),
+        alcoholQuantity: records.reduce(
+          (accumulator: number, current: LiquorRecord) => accumulator + (current.liquor.alcohol?.quantity ?? 0), 0
+        )
+      }
+    },
+    liquorRecordsLast12Hours(): LiquorRecord[] {
+      const query: recordQueryPayload = {
+        betweenNow: {
+          hour: 12
+        }
+      }
+      return this.$store.getters['records/queryLiquorRecords'](query, this.$dayjs)
+    },
+  }
 })
 </script>
 
--- store/records.ts
+++ store/records.ts
@@ -3,18 +3,47 @@ import {LiquorRecord} from "~/model/liquorRecord";
 import {WaterRecord} from "~/model/waterRecord";
 import {v4 as uuidv4} from "uuid";
 import {Liquor} from "~/model/liquor";
-
+import dayjs from 'dayjs'
 
 export const state = () => ({
   records: [] as (LiquorRecord | WaterRecord)[]
 })
 
+export interface recordQueryPayload {
+  betweenNow?: {
+    // TODO: 今のところ、上から順に適用されるけどいい感じにやりたいよね
+    day?: number
+    hour?: number
+  }
+}
+
 export type RootState = ReturnType<typeof state>
+
+type dayjsType = typeof dayjs
 
 export const getters: GetterTree<RootState, RootState> = {
   allRecords: state => state.records,
-  liquorRecords: state => state.records.filter(({type}) => type === "liquor"),
-  waterRecords: state => state.records.filter(({type}) => type === "water")
+  liquorRecords: ({records}): LiquorRecord[] => {
+    return records.filter((record): record is LiquorRecord => record.type === "liquor")
+  },
+  waterRecords: ({records}): WaterRecord[] => {
+    return records.filter((record): record is WaterRecord => record.type === "water")
+  },
+  queryLiquorRecords: (_, getters) => (payload: recordQueryPayload, dayjs: dayjsType): LiquorRecord[] => {
+    let before: dayjs.Dayjs;
+    if (payload.betweenNow?.day) {
+      before = dayjs().subtract(payload.betweenNow.day, 'day');
+    } else if (payload.betweenNow?.hour) {
+      before = dayjs().subtract(payload.betweenNow.hour, 'hour');
+    } else {
+      return [];
+    }
+
+    // 人生の終わりみたいなanyが見えたのでお気持ちasです(怪文書？)
+    return (getters.liquorRecords as LiquorRecord[]).filter(({date}) => {
+      return dayjs.unix(date).isAfter(before)
+    })
+  }
 }
 
 export const mutations: MutationTree<RootState> = {
@@ -42,6 +71,6 @@ export const actions: ActionTree<RootState, RootState> = {
         quantity: payload
       },
     })
-  }
+  },
 }
 
