922c0e6daa92a31e3d7e8a7693609dd27b1b98bc
marco <marco3jp+git@gmail.com>
2023-12-12 19:40:15 +0900

    使ってないファイルを一旦コメントアウト

--- src/model/setting.ts
+++ src/model/setting.ts
@@ -1,7 +1,5 @@
-import {Liquor} from "./liquor.ts";
-
-export interface Setting {
-  dayBorderTime?: number // default = 21600 (= 60sec * 60min * 6hour) = 6:00am)
-  disableDefaultLiquorTemplate?: boolean // default = false
-  liquorTemplates?: Array<Liquor> // default = []
-}
+// export interface Setting {
+//   dayBorderTime?: number // default = 21600 (= 60sec * 60min * 6hour) = 6:00am)
+//   disableDefaultLiquorTemplate?: boolean // default = false
+//   liquorTemplates?: Array<> // default = []
+// }
